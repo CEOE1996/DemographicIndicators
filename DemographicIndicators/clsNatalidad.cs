9540c49ed1970f1a53059e3a89eea6fbf6002631
Carlos Enrique Orozco Estrada <ceoe1996@hotmail.com>
2019-10-01 20:27:35 -0500

    Adding clsNatalidad

---
+++ DemographicIndicators/clsNatalidad.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemographicIndicators
+{
+    public class clsNatalidad
+    {
+        public int Nacimientos;
+        public int Poblacion;
+
+        public clsNatalidad()
+        {
+            this.Nacimientos = 1;
+            this.Poblacion = 1;
+        }
+
+        
+    }
+}
